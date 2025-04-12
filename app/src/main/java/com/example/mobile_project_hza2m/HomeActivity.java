eb4e743cbd19d41b4156ed926d61be5a29cf6f17
zeina mershad <mershadzeina7@gmail.com>
2025-04-13 00:05:45 +0300

    -Initial Commit
    -inserted splash activity
    -inserted home activity

---
+++ app/src/main/java/com/example/mobile_project_hza2m/HomeActivity.java
@@ -0,0 +1,34 @@
+package com.example.mobile_project_hza2m;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.mobile_project_hza2m.databinding.ActivityHomeBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityHomeBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+
+
+}}
