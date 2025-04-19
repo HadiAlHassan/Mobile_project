938e694b1bf9a62da3b5fbb6026d6676480dcbfa
mariam <mariamzaiter171@gmail.com>
2025-04-19 12:16:52 +0300

    user signup/login done

--- app/src/main/java/com/example/mobile_project_hza2m/UserDetails.java
+++ app/src/main/java/com/example/mobile_project_hza2m/UserDetails.java
@@ -1,22 +1,210 @@
 package com.example.mobile_project_hza2m;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.ui.AppBarConfiguration;
+
+
+
+import com.google.android.material.snackbar.Snackbar;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
 
 public class UserDetails extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 100;
+    private ActivityResultLauncher<Intent> galleryLauncher;
+    private ImageView imageView;
+    private EditText fname, mname,lname, phone, address, username, pass, pass1;
+    private RadioButton rb1, rb2;
+    private Button button;
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;});
+        imageView = findViewById(R.id.imageView);
+        Button button = findViewById(R.id.button);
+        button1 = findViewById(R.id.button1);
 
-        Intent sasho=getIntent();
+        fname = findViewById(R.id.fname);
+        mname = findViewById(R.id.mname);
+        lname = findViewById(R.id.lname);
+        phone = findViewById(R.id.phone);
+        address = findViewById(R.id.address);
+        username = findViewById(R.id.username);
+        pass = findViewById(R.id.pass);
+        pass1 = findViewById(R.id.pass1);
+        rb1 = findViewById(R.id.rb1);
+        rb2 = findViewById(R.id.rb2);
+        button.setOnClickListener(v -> openGallery());
+        button1.setOnClickListener(v -> validateInputs());
+    }
+    private void openGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+    private void validateInputs() {
+        String nameInput = fname.getText().toString().trim();
+        String mnamee = mname.getText().toString().trim();
+        String lnamee = lname.getText().toString().trim();
+        String phoneInput = phone.getText().toString().trim();
+        String addressInput = address.getText().toString().trim();
+        String usernameInput = username.getText().toString().trim();
+        String passwordInput = pass.getText().toString();
+        String confirmPasswordInput = pass1.getText().toString();
+
+
+        if (nameInput.isEmpty()) {
+            fname.setError("Name is required");
+            showToast("Please enter your name.");
+            return;
+        }
+        if (mnamee.isEmpty()) {
+            mname.setError("Name is required");
+            showToast("Please enter your name.");
+            return;
+        }
+        if (lnamee.isEmpty()) {
+            lname.setError("Name is required");
+            showToast("Please enter your name.");
+            return;
+        }
+
+        if (phoneInput.isEmpty()) {
+            phone.setError("Phone number is required");
+            showToast("Please enter your phone number.");
+            return;
+        }
+        if (!phoneInput.matches("^\\d{8}$")) {
+            phone.setError("Phone number should be an 8-digit number");
+            return;
+        }
+
+        if (addressInput.isEmpty()) {
+            address.setError("Address is required");
+            showToast("Please enter your address.");
+            return;
+        }
+        String emailPattern = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        if(!addressInput.matches(emailPattern)){
+            address.setError("Please align by the email pattern format");
+            return;
+        }
+
+        if (usernameInput.isEmpty()) {
+            username.setError("Username is required");
+            showToast("Please enter your username.");
+            return;
+        }
+
+        if (passwordInput.isEmpty()) {
+            pass.setError("Password is required");
+            showToast("Please enter your password.");
+            return;
+        }
+        String passpattern="^(?=.*[A-Z])(?=.*[a-zA-Z0-9]).{6,}$";
+        if(!passwordInput.matches(passpattern)){
+            pass.setError("Please note that your chosen password should be at least 6 alphanumerics long with one uppercase letter ");
+            return;
+        }
+
+
+        if (confirmPasswordInput.isEmpty()) {
+            pass1.setError("Please confirm your password");
+            showToast("Please confirm your password.");
+            return;
+        }
+
+        if (!passwordInput.equals(confirmPasswordInput)) {
+            pass1.setError("Passwords do not match");
+            showToast("Passwords mismatch.");
+            return;
+        }
+
+        if (!rb1.isChecked() && !rb2.isChecked()) {
+            showToast("Please select your age group.");
+            return;
+        }
+
+        if (rb1.isChecked()) {
+            showToast("You must be at least 18 years old to sign up.");
+            return;
+        }
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(UserDetails.this);
+        builder.setTitle("Terms and Conditions");
+        builder.setMessage("Please review and accept our Terms & Conditions before signing up:\n\n" +
+                "1. You must be at least 18 years old and legally eligible to use this app.\n\n" +
+                "2. You are responsible for your account, including all transactions and keeping your credentials secure.\n\n" +
+                "3. Illegal use is strictly prohibited — including fraud, money laundering, or unauthorized transfers.\n\n" +
+                "4. Fees may apply for specific transactions, transfers, or account services. These will be disclosed when applicable.\n\n" +
+                "5. We may suspend or terminate your account for policy violations or suspicious activity.\n\n" );
+
+        builder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                showToast("Sign-up successful! ");
+                Intent success = new Intent(UserDetails.this, UserLogin.class);
+                startActivity(success);
+            }
+        });
+
+        builder.setNegativeButton("Reject", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                showToast("Sign-up failed: You must accept the community guidelines.");
+            }
+        });
+
+        builder.setCancelable(false);
+        builder.create().show();
+    }
+
+
+    private void showToast(String message) {
+        Toast.makeText(UserDetails.this, message, Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null) {
+            Uri imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/mobile_project_hza2m/UserOrProvider.java
+++ app/src/main/java/com/example/mobile_project_hza2m/UserOrProvider.java
@@ -1,8 +1,11 @@
 package com.example.mobile_project_hza2m;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 
@@ -11,12 +14,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.ui.AppBarConfiguration;
 
+import com.example.mobile_project_hza2m.databinding.ActivityUserOrProviderBinding;
 import com.google.android.material.radiobutton.MaterialRadioButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class UserOrProvider extends AppCompatActivity {
     ImageView back;
     RadioButton rb1, rb2;
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityUserOrProviderBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +36,34 @@ public class UserOrProvider extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        Intent zeer= getIntent();
+        binding = ActivityUserOrProviderBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+                builder.setTitle("Personal Inquires");
+
+                // Set your message here
+                builder.setMessage("Shall you have any concern or question, feel free to contact us on our hotline 1297 ");
+
+                // Add OK button
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss(); // just dismiss the dialog
+                    }
+                });
+
+                // Show the dialog
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
         ImageView back= findViewById(R.id.back);
         RadioButton rb1= findViewById(R.id.rb1);
         RadioButton rb2= findViewById(R.id.rb2);
@@ -35,17 +71,25 @@ public class UserOrProvider extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent sushi= new Intent(UserOrProvider.this,HomeActivity.class);
-                startActivity(sushi);
+                Intent on= new Intent(UserOrProvider.this,HomeActivity.class);
+                startActivity(on);
             }
 
 
         });
-        rb1.setOnClickListener(new View.OnClickListener() {
+        /*.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent sashimi= new Intent(UserOrProvider.this, UserDetails.class);
-                startActivity(sashimi);
+                Intent in4= new Intent(UserOrProvider.this, UserDetails.class);
+                startActivity(in4);
+            }
+        });*/
+
+        rb1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Intent in4= new Intent(UserOrProvider.this, UserDetails.class);
+                startActivity(in4);
             }
         });
 
